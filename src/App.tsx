3530a0918315fe6f3965ccee4d30eeed40c34e9f
marcosirb <marcosrissotto@gmail.com>
2022-11-01 13:16:54 -0300

    Add react-router-dom with initial home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Layout from './layouts/layout'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/home'
 
 const App: React.FC = () => {
   return (
     <Layout>
-      Pokedex
+      <Routes>
+        <Route path='/' element={<Home/>} />
+      </Routes>
     </Layout>
   )
 }
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,11 @@
+import { Stack, Text } from '@chakra-ui/react'
+
+const Home = () => {
+  return (
+    <Stack>
+      <Text>Pokedex</Text>
+    </Stack>
+  )
+}
+
+export default Home
